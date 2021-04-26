a3db2be1abf2b7e8689a178f208ff320b6670bc1
154678794 <154678794@qq.com>
2021-04-26 16:24:00 +0800

    origin

---
+++ src/main/java/com/example/lioveecommunityj/LioveeCommunityJApplication.java
@@ -0,0 +1,13 @@
+package com.example.lioveecommunityj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LioveeCommunityJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LioveeCommunityJApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lioveecommunityj/LioveeCommunityJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lioveecommunityj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LioveeCommunityJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
