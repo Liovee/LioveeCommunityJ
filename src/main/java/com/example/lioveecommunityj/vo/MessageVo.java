86c0120bc96974a19cf80f6204fafcdacd8f9687
154678794 <154678794@qq.com>
2021-05-05 22:09:11 +0800

    MessageVo bug修复

--- src/main/java/com/example/lioveecommunityj/vo/MessageVo.java
+++ src/main/java/com/example/lioveecommunityj/vo/MessageVo.java
@@ -2,12 +2,25 @@ package com.example.lioveecommunityj.vo;
 
 import com.example.lioveecommunityj.entity.MessageEntity;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
+import java.util.Date;
+
+
 public class MessageVo extends MessageEntity {
+
     @ApiModelProperty("文件")
     private MultipartFile file;
 
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
+
+    public MessageVo(String content, Date publishTime, Long userId, String file_path, Integer flag) {
+        super(content, publishTime, userId, file_path, flag);
+    }
 }
